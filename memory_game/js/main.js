d5c79e42e2cfd45134a9d3d9a9089b276ea73e25
br305893 <ben.rosner.williamsburg@gmail.com>
2019-05-07 14:09:08 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,14 +1,17 @@
-var cards = ["queem", "queen", "king", "king"];
+var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
+function checkForMatch() {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+}
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
+function flipCard(cardId) {
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
 if (cardsInPlay.length === 2){
 	if (cardsInPlay[0] === cardsInPlay[1]){
 		alert("You found a match!");
@@ -16,5 +19,8 @@ if (cardsInPlay.length === 2){
 	else {
 		alert("Sorry, try again.");
 	}
+	checkForMatch();
+ }
 }
-
+flipCard(0);
+flipCard(2);
